afab305ce53930b24034337a3c2c95d483d50c6e
Lucas de Oliveira <lucas.g.oliveira1998@outlook.com>
2023-04-30 12:36:19 -0300

    Adicionando codigo para verificar a idade do usuario

--- src/br/com/devcaotics/HelloWorld.java
+++ src/br/com/devcaotics/HelloWorld.java
@@ -1,8 +1,22 @@
 package br.com.devcaotics;
 
+import java.util.Scanner;
+
 public class HelloWorld
 {
 	public static void main(String[] args)
 	{
+		Scanner entrada = new Scanner(System.in);
+		int idade;
+		
+		System.out.print("Digite a sua idade: ");
+		idade = Integer.parseInt(entrada.nextLine());
+		
+		if (idade >= 18)
+			System.out.println("Você é maior de idade.");
+		else
+			System.out.println("Você não é maior de idade. A-ha!!!");
+		
+		entrada.close();
 	}
 }
