38648d3646ec35b113e6ebd659140165eb0a062d
Lucas de Oliveira <lucas.g.oliveira1998@outlook.com>
2023-04-30 12:24:43 -0300

    Modificando estilo de codigo

--- src/br/com/devcaotics/HelloWorld.java
+++ src/br/com/devcaotics/HelloWorld.java
@@ -1,7 +1,9 @@
 package br.com.devcaotics;
 
-public class HelloWorld {
-	public static void main(String[] args) {
+public class HelloWorld
+{
+	public static void main(String[] args)
+	{
 		System.out.println("Hello World!!!");
 	}
 }
